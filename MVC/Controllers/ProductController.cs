e0c5f944b85ddbe97a03e5af018b48949b8f0735
nuhan20 <nuhan.digonto@gmail.com>
2023-08-06 12:17:17 +0600

    Add project files.

---
+++ MVC/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<ProductModel> emp;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Product").Result;
+            emp = response.Content.ReadAsAsync<IEnumerable<ProductModel>>().Result;
+
+            return View(emp);
+        }
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if(id == 0)
+            return View(new ProductModel());
+
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Product/"+id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<ProductModel>().Result);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(ProductModel emp)
+        {
+            if (emp.Id == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Product", emp).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Product/"+emp.Id,emp).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+
+            }
+            
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Product/"+id.ToString()).Result;
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Webapi/Controllers/ProductController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Webapi.Models;
+
+namespace Webapi.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private productModel db = new productModel();
+
+        // GET: api/Product
+        public IQueryable<Product> GetProduct()
+        {
+            return db.Product;
+        }
+
+        // GET: api/Product/5
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult GetProduct(int id)
+        {
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // PUT: api/Product/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProduct(int id, Product product)
+        {
+           
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Product
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult PostProduct(Product product)
+        {
+           
+            db.Product.Add(product);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = product.Id }, product);
+        }
+
+        // DELETE: api/Product/5
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult DeleteProduct(int id)
+        {
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            db.Product.Remove(product);
+            db.SaveChanges();
+
+            return Ok(product);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductExists(int id)
+        {
+            return db.Product.Count(e => e.Id == id) > 0;
+        }
+    }
+}
